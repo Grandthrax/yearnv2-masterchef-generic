7b0123083672d2d4cc8ce49f5def6e969c0c3d6c
Sam Priestley <sfpriestley@hotmail.co.uk>
2021-03-07 16:29:05 +0000

    tests

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,21 +1,23 @@
 import brownie
 from brownie import Contract
+from useful_methods import genericStateOfVault, genericStateOfStrat
+import random
 
 
-def test_operation(accounts, token, vault, strategy, strategist, amount, whale):
+def test_normal_activity((accounts, token, vault, strategy, strategist, amount, whale):
 
     amount = 1*1e18
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": accounts[0]})
     vault.deposit(amount, {"from": accounts[0]})
     assert token.balanceOf(vault.address) == amount
-
+    
     # harvest
     strategy.harvest()
     assert token.balanceOf(strategy.address) == amount
 
-    # tend()
-    strategy.tend()
+    for i in range(15):
+        waitBlock = random.randint(10, 50)
 
     # withdrawal
     vault.withdraw({"from": accounts[0]})
